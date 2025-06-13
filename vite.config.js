5e2cb82b9f01bdbdb3e641cdd75d3fd1327abc31
oceanicabi13 <abiyassharamadhan@gmail.com>
2025-06-14 00:46:06 +0700

    minor fixes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import autoprefixer from 'autoprefixer'
-import flowbiteReact from "flowbite-react/plugin/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), autoprefixer(), flowbiteReact()],
+  plugins: [react()], // ✅ Only valid Vite plugins should go here
 })
