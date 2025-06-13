58781bf264e504a022be2234d37ea792ecdc1fc2
oceanicabi13 <abiyassharamadhan@gmail.com>
2025-06-14 00:53:45 +0700

    minor fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,24 @@
 import { Button } from "flowbite-react";
 
+
 function App() {
-  return <footer class="p-4 bg-white md:p-8 lg:p-10 dark:bg-gray-800">
+  return (
+    <div data-aos="fade-up">
+  <footer class="p-4 bg-white md:p-8 lg:p-10 dark:bg-gray-800">
+    
   <div class="mx-auto max-w-screen-xl text-center">
       <h1 className="text-6xl mt-8 font-bold text-white mb-8">
           CryptoWatch    
       </h1>
       <p class="my-6 text-gray-500 dark:text-gray-400">Created by Abiyassha Ramadhan<br></br>What's up another project testing of mine that involves API, not the best one you'd see but it's something <br></br> Please note that there isn't much of Crypto because of API request limit, so this tracker can searched for 8 pages ONLY.<br></br>Made with ReactJS + Tailwindcss, Glaze whenever you want and have a nice day!❤️</p>
-      <img src="./image/logo.png"
+      <img src="/image/logo.png"
         className="w-30 h-30 mx-auto max-w-2x1 lg:max-w-2x1 flex items-center rounded-full border-4 border-black transform -translate-y-3 "
       ></img>
       <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2021-2025 <a href="#" class="hover:underline">Boom</a> All Rights Reserved.</span>
   </div>
 </footer>
+</div>
+
+  )
 }
 export default App;
