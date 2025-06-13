3fc063d391e8fbd3e157b85611a03b4522ef1d96
oceanicabi13 <abiyassharamadhan@gmail.com>
2025-06-14 01:08:02 +0700

    Image fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,9 +9,9 @@ function App() {
           CryptoWatch    
       </h1>
       <p class="my-6 text-gray-500 dark:text-gray-400">Created by Abiyassha Ramadhan<br></br>What's up another project testing of mine that involves API, not the best one you'd see but it's something <br></br> Please note that there isn't much of Crypto because of API request limit, so this tracker can searched for 8 pages ONLY.<br></br>Made with ReactJS + Tailwindcss, Glaze whenever you want and have a nice day!❤️</p>
-      <img src="/image/Logo.png"
-        className="w-30 h-30 mx-auto max-w-2x1 lg:max-w-2x1 flex items-center rounded-full border-4 border-black transform -translate-y-3 "
-      ></img>
+      <img src="/images/Logo.png"
+        className="w-30 h-30 mx-auto max-w-2x1 lg:max-w-2x1 flex items-center rounded-full border-4 border-black transform -translate-y-3"
+      />
       <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2021-2025 <a href="#" class="hover:underline">Boom</a> All Rights Reserved.</span>
   </div>
 </footer>
